eeb23d942d1c76169b0c42236c7bd4ce1394faac
Angelique Weger <angelique@detroittrading.com>
2016-10-25 19:56:46 -0400

    Turned on carousel autoslide; added FB preview image and OG tags.

--- js/proj.js
+++ js/proj.js
@@ -1,96 +1,96 @@
-var mobileMenu = $( '#js-mobile-menu' );
-var navMenu = $( '#js-navigation-menu' );
-var navMenuItem = $( '#js-navigation-menu a' );
+var mobileMenu = $('#js-mobile-menu');
+var navMenu = $('#js-navigation-menu');
+var navMenuItem = $('#js-navigation-menu a');
 var menuToggle = mobileMenu.unbind();
 
-menuToggle.on( 'click', function( e ) {
+menuToggle.on('click', function (e) {
     e.preventDefault();
-    navMenu.slideToggle( function() {
-        if ( navMenu.is( ':hidden' ) ) {
-            navMenu.removeAttr( 'style' );
+    navMenu.slideToggle(function () {
+        if (navMenu.is(':hidden')) {
+            navMenu.removeAttr('style');
         }
-    } );
-} );
+    });
+});
 
-navMenuItem.on( 'click', function() {
-    if ( mobileMenu.is( ':visible' ) ) {
-        navMenu.slideToggle( function() {
-            if ( navMenu.is( ':hidden' ) ) {
-                navMenu.removeAttr( 'style' );
+navMenuItem.on('click', function () {
+    if (mobileMenu.is(':visible')) {
+        navMenu.slideToggle(function () {
+            if (navMenu.is(':hidden')) {
+                navMenu.removeAttr('style');
             }
-        } );
+        });
     }
-} );
+});
 
-$( function() {
+$(function () {
     // Generic selector to be used anywhere
-    $( '.js-scroll-to' ).click( function( e ) {
+    $('.js-scroll-to').click(function (e) {
 
         // Get the href dynamically
-        var destination = $( this ).attr( 'href' );
+        var destination = $(this).attr('href');
 
         // Set defaults
         var thespeed = 1000;
-        var offset = $( destination ).offset().top - 65;
+        var offset = $(destination).offset().top - 65;
 
         // Prevent href=“#” link from changing the URL hash (optional)
         //		e.preventDefault();
 
         // Animate scroll to destination
-        $( 'html, body' ).animate( {
+        $('html, body').animate({
             scrollTop: offset
-        }, thespeed, 'swing' );
-    } );
-} );
-$( '#js-map-container' )
-    .click( function() {
-        $( this ).find( 'iframe' ).addClass( 'is-clicked' );
-    } )
-    .mouseleave( function() {
-        $( this ).find( 'iframe' ).removeClass( 'is-clicked' );
-    } );
-$( '.sliderWrapper' ).slick( {
-    //autoplay: true,
-    //	autoplaySpeed: 2000,
+        }, thespeed, 'swing');
+    });
+});
+$('#js-map-container')
+    .click(function () {
+        $(this).find('iframe').addClass('is-clicked');
+    })
+    .mouseleave(function () {
+        $(this).find('iframe').removeClass('is-clicked');
+    });
+$('.sliderWrapper').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
     mobileFirst: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    responsive: [ {
-            breakpoint: 599,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 899,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3
-            }
+    responsive: [{
+        breakpoint: 599,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
         }
+    },
+    {
+        breakpoint: 899,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3
+        }
+    }
     ]
-} );
-$( function() {
-    $( "#credits" ).on( "change", function() {
-        if ( $( this ).is( ":checked" ) ) {
-            $( "body" ).addClass( "modal-is-open" );
+});
+$(function () {
+    $("#credits").on("change", function () {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-is-open");
         } else {
-            $( "body" ).removeClass( "modal-is-open" );
+            $("body").removeClass("modal-is-open");
         }
-    } );
-    $( "#coc" ).on( "change", function() {
-        if ( $( this ).is( ":checked" ) ) {
-            $( "body" ).addClass( "modal-is-open" );
+    });
+    $("#coc").on("change", function () {
+        if ($(this).is(":checked")) {
+            $("body").addClass("modal-is-open");
         } else {
-            $( "body" ).removeClass( "modal-is-open" );
+            $("body").removeClass("modal-is-open");
         }
-    } );
-    $( ".modalMask, .modalClose" ).on( "click", function() {
-        $( ".js-modal-state:checked" ).prop( "checked", false ).change();
-    } );
+    });
+    $(".modalMask, .modalClose").on("click", function () {
+        $(".js-modal-state:checked").prop("checked", false).change();
+    });
 
-    $( ".modalInner" ).on( "click", function( e ) {
+    $(".modalInner").on("click", function (e) {
         e.stopPropagation();
-    } );
-} );
+    });
+});
